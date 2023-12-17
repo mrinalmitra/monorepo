7ee7173a18f4cc47a2f52ed7f669e4f7947a81e1
mrinalgithub <mrinalgithub@gmail.com>
2023-12-17 20:08:36 +0530

    chore: playground utilizing both packages react and scss

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import {Button} from '@ds.e/react'
+import '@ds.e/scss/lib/Buttons.css'
+
+ReactDOM.render(
+    <Button label='Example Button'/>,
+    document.querySelector('#root')
+)
